784aa56f50d6968007d4d0f6c5339c00d375658b
Michael080 <mrollin808@gmail.com>
2020-03-29 21:12:13 -0600

    Create method that squares a given number

---
+++ 1-Intro_to_Computer_Science/Exercises/exercises.rb
@@ -0,0 +1,15 @@
+# =========================================================================================
+# 1. Imagine that you are creating a pocket calculator. You have created the functionality
+# for all the buttons except x 2 , the button that squares a number, and exp, which allows
+# you to calculate base exponent , where exponent is an integer. You may use any other
+# functionality a calculator would normally have: for example, (+, -, *, /, =).
+# a. Create the functionality for the x 2 button.
+# b. Create the functionality for the exp button.
+
+def square(num)
+  num * num
+end
+
+p square(2)
+p square(5)
+p square(4)
