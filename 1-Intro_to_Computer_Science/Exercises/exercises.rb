69da5bc55be71968db703a43df0cfc30f6af8cca
Michael080 <mrollin808@gmail.com>
2020-04-06 19:20:00 -0600

    Answer questions

--- 1-Intro_to_Computer_Science/Exercises/exercises.rb
+++ 1-Intro_to_Computer_Science/Exercises/exercises.rb
@@ -5,7 +5,7 @@
 # functionality a calculator would normally have: for example, (+, -, *, /, =).
 # a. Create the functionality for the x 2 button.
 # b. Create the functionality for the exp button.
-puts "--------------------------------------
+puts "----------------------------------------------------------------------------------------
 1.)"
 
 def square(num)
@@ -34,7 +34,7 @@ p exp(5, 3)
 # many steps does your algorithm take to complete when n = 100? How many
 # steps does it take when n = 1,000?
 #
-puts "--------------------------------------
+puts "----------------------------------------------------------------------------------------
 2a.)"
 
 def summerizer
@@ -61,7 +61,7 @@ summerizer
 
 #     b. Can you create an algorithm like Gauss’s where the number of steps does not
 # depend on n?
-puts "--------------------------------------
+puts "----------------------------------------------------------------------------------------
 2a.)"
 # Guassian thingamajig:
 
@@ -83,5 +83,81 @@ num_set_1.each do |num|
 end
 
 p "The sum of the first 100 whole numbers: #{result}"
+
+puts "----------------------------------------------------------------------------------------
+3.)"
+# 3. A palindrome is a word or phrase that reads the same way forward and backward,
+# like “racecar.” Describe a sequence of steps that determines if a word or phrase is a
+# palindrome.
+
+#     1. Take next two letters which have not yet been compared, one from the beginning of the
+# word and the other from the opposite
+# side
+# 2. Compare letters
+# 3a. if true - repeat step one
+# 3b. if false - word is not a palindrome
+# If there are no more letters to check and they all evaluated
+# true: The word IS a palindrome! Stop execution.
+
+    puts "----------------------------------------------------------------------------------------
+4.)"
+# 4. Consider the three mazes shown in Figure 1-3. Describe two different algorithms
+#     for solving a maze. Discuss advantages and disadvantages of each algorithm. Then
+# look at the maze and predict which algorithm will complete first. See if your pre‐
+# dictions were correct by applying your algorithms to the mazes.
+
+# Turn left by default until there are no options to turn left
+#  while saving the locations of any alternative paths (right turns or forward paths).
+# If there is a right turn take it and SAVE location as a
+#     NEW route and revert back to default turning behavior
+# If there is a dead end go to last turn and take alternative
+# path if there is one. If there isn't an alternative path go
+#   back to back another 'step' and take that alternative path.
+#     # In the dead-end case where all backsteps have no alternative paths. Trace back
+# to the last right turn that
+#     was passed and take that route.
+#     # If there aren't any right turns to trace back to go to
+# the last saved route and repeat this backtracking process
+
+
+
+
+# When all left turns have been exhausted return to the first
+# left turn of that particular route
+
+
+# if (no wall)
+#   move forward
+#   if (wall)
+#     (opening on left side) ?
+#         turn left and move forward
+#   elsif
+#   turn right and save as new route
+#
+#
+#     if (wall) && (no alternate paths)
+#       go back to last right turn which was passed on that route
+#     else
+#       go back to last route && take alternate path
+
+# This answer is probably full of errors
+
+
+# 5. Figure 1-4 shows an alternative way to represent an algorithm. (Note: we introduce
+#       this construct in detail later on. If it looks too intimidating, skip it until after you’ve
+#       read Chapter 4.)
+#       a. Starting at the circle labeled “Start” work your way through the figure. What is
+#       the purpose of this algorithm?
+#       b. Translate the figure into simple language. Note that a diamond in the figure
+#       represents a condition that may be true or false.
+# a. This is an algorithm for providing change at POS.
+#
+# b.
+#     1. The value of C is established as the remainder of money paid - const
+#     2. If the C is greater than 1 give one dollar to customer and subtract 1 from C to update
+#     change due.
+#     - Check condition again until C is less than 1.
+#     3. Perform the same conditionals on C for quarters, dimes, and pennies until C is not
+#     greater than or equal to .01.
 
 
